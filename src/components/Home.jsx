f6a3f7b6a075008652cf2b6fd19e1844922613bd
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-21 14:45:18 +0100

    Part. 1 finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [pokemon, setPokemon] = useState({});
+  const [number, setNumber] = useState(1);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${number}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setPokemon(res);
+      });
+  }, [number]);
+
+  const randomNumber = () => {
+    return setNumber(Math.floor(Math.random() * 100) + 1);
+  };
+
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={randomNumber}>RANDOM</button>
+      <ul>
+        <li>{pokemon.name}</li>
+        <li>{pokemon.weight} cm</li>
+        <li>{pokemon.height} kg</li>
+        {/* <li>{pokemon.types.type.name}</li> */}
+      </ul>
     </div>
   );
 }
