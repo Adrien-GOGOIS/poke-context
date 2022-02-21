49e37401a39e7ac72c652088948e7816c11874ce
Adrien GOGOIS <ad.gogois@gmail.com>
2022-02-21 14:23:45 +0100

    Creating form with react-hook-form in Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,39 @@
+import { useForm } from "react-hook-form";
+
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log("Formulaire soumis", data);
+
   return (
     <div>
       <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("username")}
+          type="text"
+          name="username"
+          id="username"
+          required
+          maxLength={15}
+          placeholder="Username"
+        />
+        <input
+          {...register("password")}
+          type="password"
+          name="password"
+          id="password"
+          required
+          minLength={6}
+          placeholder="Password"
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
